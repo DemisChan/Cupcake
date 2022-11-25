6ec5854ff3ba75d0f5a89784865173573e6f3163
Demis Lavrentidis <demis.lavrentidis@openbet.com>
2022-11-25 17:41:35 +0000

    * added send order functionality with intent * added plurals for cupcakes in string resources

--- app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -16,18 +16,14 @@
 package com.example.cupcake
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentPickupBinding
 import com.example.cupcake.model.OrderViewModel
-import java.text.SimpleDateFormat
-import java.util.*
 
 /**
  * [PickupFragment] allows the user to choose a pickup date for the cupcake order.
